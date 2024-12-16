dc662f22414aa050edbf90cc94f6a937c6a9348f
Haneol LEE <skqieum@gmail.com>
2024-12-17 06:31:19 +0900

    Update QuestionPage.js

--- it-mbti/src/pages/QuestionPage.js
+++ it-mbti/src/pages/QuestionPage.js
@@ -4,36 +4,36 @@ import styled from "styled-components";
 import Questions from "../components/Questions";
 import questions from "../data/questions.json"; 
 
-const Container = styled.div
+const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
   background: linear-gradient(180deg, #E8EAF3 10%, #A0B7E1 40%, #4A79D1 90%);
-;
+`;
 
-const ProgressBarContainer = styled.div
+const ProgressBarContainer = styled.div`
   width: 80%;
   height: 20px;
   background-color: #ddd;
   border-radius: 10px;
   margin-bottom: 20px;
   overflow: hidden;
-;
+`;
 
-const Progress = styled.div
+const Progress = styled.div`
   height: 100%;
   width: ${({ percentage }) => percentage}%;
   background-color: #4A79D1;
   transition: width 0.3s ease-in-out;
-;
+`;
 
-const ButtonContainer = styled.div
+const ButtonContainer = styled.div`
   margin-top: 30px;
-;
+`;
 
-const NavButton = styled.button
+const NavButton = styled.button`
   padding: 10px 20px;
   font-size: 2rem;
   margin: 20px;
@@ -46,34 +46,36 @@ const NavButton = styled.button
   &:hover {
     background-color: #ddd;
   }
-;
+`;
 
 function QuestionPage() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [scores, setScores] = useState({});
   const navigate = useNavigate();
 
   const handleAnswer = (type, score) => {
     setScores((prevScores) => {
       const updatedScores = {
         ...prevScores,
-        [type]: (prevScores[type] || 0) + score, 
+        [type]: (prevScores[type] || 0) + score,
       };
 
       if (currentIndex < questions.length - 1) {
-        setCurrentIndex(currentIndex + 1); 
+        setCurrentIndex(currentIndex + 1);
       } else {
-        navigate("/loading", { state: { scores: updatedScores } }); 
+        navigate("/loading", { state: { scores: updatedScores } });
       }
 
-      return updatedScores; 
+      return updatedScores;
     });
   };
 
   return (
     <Container>
       <ProgressBarContainer>
-        <Progress percentage={((currentIndex + 1) / questions.length) * 100} 
-        role="progressbar"
+        <Progress
+          percentage={((currentIndex + 1) / questions.length) * 100}
+          role="progressbar"
         />
       </ProgressBarContainer>
       <Questions
